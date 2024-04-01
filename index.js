18e1afabec29591c9cbc9e851365a7062482921e
Luke S. <lukestroehlein18@gmail.com>
2024-04-01 15:27:39 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+//TODO: Write code that will execute the commandline.js file in the command line
+
+const commandLine= require('./lib/commandline.js');
+const command = new commandLine();
+command.run();
